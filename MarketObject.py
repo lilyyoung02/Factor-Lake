77de662de8b17f9c22fca8d6ebe91cd02806645c
lilyyoung02 <101677793+lilyyoung02@users.noreply.github.com>
2025-04-15 12:20:20 -0400

    Update MarketObject.py

--- MarketObject.py
+++ MarketObject.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 from LoggerConfiguration import get_logger
 import logging
-effective_level = logging.getLogger().getEffectiveLevel()
-logger = get_logger(__name__, level=effective_level)
+logger = get_logger(__name__, level=logging.getLogger().getEffectiveLevel())
+logger.debug("Logger configured in FactorLake")
 
 ### CREATING FUNCTION TO LOAD DATA ###
 def load_data():
