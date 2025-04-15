704be8713a40b20e4d6895727151026a2968ba09
lilyyoung02 <101677793+lilyyoung02@users.noreply.github.com>
2025-04-15 11:52:57 -0400

    Update LoggerConfiguration.py

--- LoggerConfiguration.py
+++ LoggerConfiguration.py
@@ -1,12 +1,15 @@
 import logging
 
-def get_logger(name=None, level=None):
+def get_logger(name=None, level=logging.INFO):
     logger = logging.getLogger(name)
+    
+    # If no handlers, set one
     if not logger.handlers:
         handler = logging.StreamHandler()
         formatter = logging.Formatter('%(message)s')
         handler.setFormatter(formatter)
+        handler.setLevel(level)  
         logger.addHandler(handler)
-    if level is not None:
-        logger.setLevel(level)
+    
+    logger.setLevel(level)  # Logger level
     return logger
